1b3a89eac39c4857edff7484bd62115aa8361011
nouman557 <nouman.saeed@o3interfaces.com>
2021-11-19 16:00:42 +0500

    Header NavBar design update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  navBarMobileOpen = false
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openNavBarForMobileView() {
+    this.navBarMobileOpen = !this.navBarMobileOpen
   }
 
 }
