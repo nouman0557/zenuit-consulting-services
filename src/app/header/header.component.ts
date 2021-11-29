c46048fc3b4cd5318762afab0a59e6f2394d4f92
nouman557 <nouman.saeed@o3interfaces.com>
2021-11-29 18:08:50 +0500

    header updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 
 @Component({
@@ -12,12 +12,35 @@ export class HeaderComponent implements OnInit {
   constructor(public _router: Router) { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true); //third parameter
+
   }
 
   openNavBarForMobileView() {
     this.navBarMobileOpen = !this.navBarMobileOpen
   }
 
+  scrollDown = false;
+  topPosToStartShowing = 100;
+  @HostListener('scroll')
 
+  onActive() {
+    window.scroll(0, 0)
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+
+  }
+
+  scroll = (): void => {
+    this.scrollDown = true;
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.scrollDown = true;
+    } else {
+      this.scrollDown = false;
+    }
+  };
 
 }
