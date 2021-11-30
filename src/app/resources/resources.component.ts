9bdfd846ff71ba2b7ff019c0226d2665cd255f6a
nouman557 <nouman.saeed@o3interfaces.com>
2021-11-30 17:46:50 +0500

    resorces page web design completed

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -10,7 +10,16 @@ export class ResourcesComponent implements OnInit {
 
 
   constructor(public commonSerice: CommomService) { }
+  resources = [
+    { imgUrl: '"../../assets/images/resources/res-img.png', title: 'Lorem ipsum dolor sit amet, consectetur ', description: 'Cyber attacks are so commonplace that they have become a part of the business world. The result is detrimental to organizations, alongside their customers, leads' },
+    { imgUrl: '"../../assets/images/resources/res-img.png', title: 'Lorem ipsum dolor sit amet, consectetur ', description: 'Cyber attacks are so commonplace that they have become a part of the business world. The result is detrimental to organizations, alongside their customers, leads' },
+    { imgUrl: '"../../assets/images/resources/res-img.png', title: 'Lorem ipsum dolor sit amet, consectetur ', description: 'Cyber attacks are so commonplace that they have become a part of the business world. The result is detrimental to organizations, alongside their customers, leads' },
+    { imgUrl: '"../../assets/images/resources/res-img.png', title: 'Lorem ipsum dolor sit amet, consectetur ', description: 'Cyber attacks are so commonplace that they have become a part of the business world. The result is detrimental to organizations, alongside their customers, leads' },
+    { imgUrl: '"../../assets/images/resources/res-img.png', title: 'Lorem ipsum dolor sit amet, consectetur ', description: 'Cyber attacks are so commonplace that they have become a part of the business world. The result is detrimental to organizations, alongside their customers, leads' },
+    { imgUrl: '"../../assets/images/resources/res-img.png', title: 'Lorem ipsum dolor sit amet, consectetur ', description: 'Cyber attacks are so commonplace that they have become a part of the business world. The result is detrimental to organizations, alongside their customers, leads' },
+    { imgUrl: '"../../assets/images/resources/res-img.png', title: 'Lorem ipsum dolor sit amet, consectetur ', description: 'Cyber attacks are so commonplace that they have become a part of the business world. The result is detrimental to organizations, alongside their customers, leads' }
 
+  ]
   ngOnInit(): void {
     $('html, body').animate({
       scrollTop: 0,
