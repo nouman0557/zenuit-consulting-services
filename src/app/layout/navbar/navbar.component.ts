5033bf780f28e6e94094a594d3e813bed76e82b3
nouman557 <nouman.saeed@o3interfaces.com>
2021-12-07 18:26:33 +0500

    new NavBar update part I done

---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,46 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonService } from '../../core/data-services/common.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  navBarMobileOpen = false
+
+  constructor(public _router: Router, public commonSerice: CommonService) { }
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true); //third parameter
+
+  }
+
+  openNavBarForMobileView() {
+    this.navBarMobileOpen = !this.navBarMobileOpen
+  }
+
+  scrollDown = false;
+  topPosToStartShowing = 100;
+  @HostListener('scroll')
+
+  onActive() {
+    window.scroll(0, 0)
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+
+  }
+
+  scroll = (): void => {
+    this.scrollDown = true;
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.scrollDown = true;
+    } else {
+      this.scrollDown = false;
+    }
+  };
+}
